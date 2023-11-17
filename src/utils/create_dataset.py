a7d773ade0aedaa39451bdd86262e92107203aae
axeld <axel.dovskog@outlook.com>
2023-11-17 19:25:17 +0100

    Multiprocessing for create_dataset

--- src/utils/create_dataset.py
+++ src/utils/create_dataset.py
@@ -2,6 +2,8 @@ from PIL import Image
 from process_image import process_image
 import os
 import numpy as np
+import multiprocessing as mp
+from multiprocessing import Process
 
 def create_dataset(images_path: str,
                    labels: list[int],
@@ -9,8 +11,12 @@ def create_dataset(images_path: str,
                    new_path: str = "datasets",
                    filename: str = "dataset",
                    ):
+
     paths = list(map(lambda path: os.path.join(images_path, path), os.listdir(images_path)))
-    images = np.array(list(map(lambda path: process_image(path, image_dimension), paths)))
+
+    pool = mp.Pool(3)
+    processes = [pool.apply_async(process_image, args=(path,image_dimension,)) for path in paths]
+    images = np.array([p.get() for p in processes])
 
     print(images.shape)
 
--- src/utils/process_image.py
+++ src/utils/process_image.py
@@ -4,6 +4,5 @@ from PIL import Image
 def process_image(path: str, dimensions: tuple[int, int]):
         img = Image.open(path)
         img = img.resize(dimensions)
-        img.show()
         img = np.array(img)
         return img
